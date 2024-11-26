851cc9e1b47410b14d04bb9a50303663ef24c754
dicespedes <deo.cespedes@gmail.com>
2024-11-26 16:04:14 -0600

    Ajustes al primer test

--- Calculadora/PrincipalTest.cs
+++ Calculadora/PrincipalTest.cs
@@ -5,17 +5,26 @@ namespace Calculadora
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
 
         [Test]
-        public void Test1()
+        public void Suma()
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Minimize();
             driver.Navigate().GoToUrl("https://testsheepnz.github.io/BasicCalculator.html");
+
+            driver.FindElement(By.Id("number1Field")).SendKeys("1");
+            driver.FindElement(By.Id("number2Field")).SendKeys("1");
+            driver.FindElement(By.Id("calculateButton")).Click();
+
+
+            driver.Close();
+            driver.Quit();
+            
 
             //Assert.Pass();
         }
